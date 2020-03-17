63920cb384e074ca7dd1ee9e382313ee16b30d2b
Omar Faruk Munna <omarmunna67@outlook.com>
2020-03-17 13:32:18 +0600

    Add project files.

---
+++ MyCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
